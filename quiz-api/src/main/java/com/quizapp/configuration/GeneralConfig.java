a702cb88a3917cab84931759f40fc1bf60fffef0
celilcanidiz <celilcanidiz@gmail.com>
2024-01-23 18:59:39 +0300

    bean annotation eklendi

--- quiz-api/src/main/java/com/quizapp/configuration/GeneralConfig.java
+++ quiz-api/src/main/java/com/quizapp/configuration/GeneralConfig.java
@@ -1,11 +1,13 @@
 package com.quizapp.configuration;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration("gConfig")
 public class GeneralConfig {
 
+    @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
