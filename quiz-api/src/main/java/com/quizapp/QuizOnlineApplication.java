7791b5fd113d74a0cfdcddce508c4abed3a240e5
celilcanidiz <celilcanidiz@gmail.com>
2024-01-23 18:59:17 +0300

    main fonksiyonu eklendi

--- quiz-api/src/main/java/com/quizapp/QuizOnlineApplication.java
+++ quiz-api/src/main/java/com/quizapp/QuizOnlineApplication.java
@@ -1,2 +1,13 @@
-package com.quizapp;public class QuizOnlineApplication {
+package com.quizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizOnlineApplication.class, args);
+    }
+
 }
