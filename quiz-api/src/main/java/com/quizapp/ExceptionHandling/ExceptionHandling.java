8d1f0a7b1a480f7665fc822050b77363c56bf6d9
celilcanidiz <celilcanidiz@gmail.com>
2024-01-23 19:00:57 +0300

    exception handling

---
+++ quiz-api/src/main/java/com/quizapp/ExceptionHandling/ExceptionHandling.java
@@ -0,0 +1,22 @@
+package com.quizapp.ExceptionHandling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandling {
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> methodExceptionHandler(Exception e){
+        String message = "Exception" + e.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(BusineessException.class)
+    public ResponseEntity<String> businessExceptionHandler(BusineessException e){
+        return new ResponseEntity<>("Business Hatası Oluştu.", HttpStatus.BAD_REQUEST);
+    }
+}
