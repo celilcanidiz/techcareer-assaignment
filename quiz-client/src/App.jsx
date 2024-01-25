afb08dd468a3ce614ef3653f1966a3898698b852
celilcanidiz <celilcanidiz@gmail.com>
2024-01-25 12:40:03 +0300

    updatequestion sayfası react routera eklendi

--- quiz-client/src/App.jsx
+++ quiz-client/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
         <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/create-quiz" element={<AddQuestion />} />
+      <Route path="/update-quiz/:id" element={<UpdateQuestion />} />
       </Routes>
       </Router>
     </main>
