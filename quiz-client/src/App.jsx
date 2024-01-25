6947823aeddbaa5b6684c3959ad3593626d0edc6
celilcanidiz <celilcanidiz@gmail.com>
2024-01-25 09:18:41 +0300

    .

--- quiz-client/src/App.jsx
+++ quiz-client/src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Home from './components/Home'
 import AddQuestion from './components/question/AddQuestion'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <AddQuestion />
-    </>
+    <main>
+      <Router>
+        <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/create-quiz" element={<AddQuestion />} />
+      </Routes>
+      </Router>
+    </main>
   )
 }
 
